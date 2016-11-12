94986e188f0d1e373b3e9e335009ebbdb3966200
Kaique Borges <rique.kaique@gmail.com>
2016-11-12 19:03:27 -0200

    Finished stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,12 +1,21 @@
 def stock_picker(array)
-  profits = []
+  best_days = [0,1]
+  best_profit = 0
+
   array.each_with_index do |buy_price, buy_index|
-    (buy_price...array.length).each_with_index do |sell_price, sell_index|
-      profits << sell_price - buy_price
+
+    (buy_index...array.length).each_with_index do |sell_index|
+      sell_price = array[sell_index]
+
+      if (sell_price - buy_price) > best_profit
+        best_profit = sell_price - buy_price
+        best_days = [buy_index, sell_index]
+      end
+
     end
   end
-  profits.max
 
+  return best_days
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
